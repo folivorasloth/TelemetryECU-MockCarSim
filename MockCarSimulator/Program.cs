f2db49b11394881601eb6e5408737d62154ff52e
joao.falcao <joaopedro35215745@gmail.com>
2026-03-17 12:45:36 -0300

    Falta acertar o acelerador e freio

--- MockCarSimulator/Program.cs
+++ MockCarSimulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MockCarSimulator;
+using System;
 using System.Threading.Tasks;
 
 namespace TelemetryECU
@@ -7,35 +8,60 @@ namespace TelemetryECU
     {
         static async Task Main(string[] args)
         {
+
+            TasksCarro carro = new TasksCarro(0, 30, 50, 0);
+
             Console.WriteLine("----------------------------------------------");
             Console.WriteLine("TelemetryECU | Mock Car Simulator");
             Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Comandos:\nL - Ligar carro\nA - Acelerar\nF - Frear\nS - Sair do simulador.");
+            Console.WriteLine("Comandos:\nL - Ligar carro\nS - Subir Marcha\nD - Descer Marcha\nA - Acelerar\nF - Frear\nQ - Sair do simulador.");
 
             bool running = true;
 
             while (running)
             {
+
+                /*
                 Console.WriteLine("Informações do carro:");
                 Console.WriteLine("RPM: " + rpm);
                 Console.WriteLine("Temperatura: " + temp);
                 Console.WriteLine("Velocidade: " + speed);
                 Console.WriteLine("Tanque combustível: " + fuel + "L");
+                */
 
                 var key = await Task.Run(() => Console.ReadKey(true).Key);
 
                 switch (key)
                 {
                     case ConsoleKey.L:
-                        await LigarCarro();
-                        break;
-                    case ConsoleKey.A:
-                        await AcelerarCarro();
-                        break;
-                    case ConsoleKey.F:
-                        await FrearCarro();
+                        await carro.LigarCarro();
+                        Console.WriteLine(carro);
                         break;
                     case ConsoleKey.S:
+                        await carro.SubirMarcha();
+                        break;
+                    case ConsoleKey.D:
+                         /*await carro.DescerMarcha();
+                        break; */
+                    case ConsoleKey.A:
+                        await carro.AcelerarCarro();
+
+                        // Limpa a tela para que a telemetria apareça sempre no topo
+                        Console.Clear();
+
+                        Console.WriteLine("----------------------------------------------");
+                        Console.WriteLine("TelemetryECU | Mock Car Simulator");
+                        Console.WriteLine("----------------------------------------------");
+
+                        // Chama o seu override ToString()
+                        Console.WriteLine(carro);
+
+                        Console.WriteLine("\n[Segure A para acelerar | F para frear | S para sair]");
+                        break;
+                    case ConsoleKey.F:
+                        await carro.FrearCarro();
+                        break;
+                    case ConsoleKey.Q:
                         running = false;
                         Console.WriteLine("Saindo do simulador...");
                         break;  
--- MockCarSimulator/Tasks.cs
+++ MockCarSimulator/Tasks.cs
@@ -12,6 +12,7 @@ namespace MockCarSimulator
         public double Temp { get; set; }
         public double Fuel { get; set; }
         public int Speed { get; set; }
+        public int Marcha { get; set; }
 
         public TasksCarro(int rpm, double temp, double fuel, int speed)
         {
@@ -27,7 +28,118 @@ namespace MockCarSimulator
             await Task.Delay(5000);
             Rpm = 900;
             Temp = 70;
-
+            Fuel -= 0.5;
+            Console.WriteLine("\nCarro ligado!");
         }
+
+        public async Task SubirMarcha()
+        {
+            if (Marcha < 5)
+            {
+                Marcha++;
+                Console.WriteLine($"Subindo marcha para: {Marcha}");
+
+                if(Marcha > 1)
+                {
+                    Rpm -= 3000;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Marcha máxima atingida.");
+            }
+        }
+
+        public async Task AcelerarCarro()
+        {
+
+            //Verifica se o usuário tentou sair com o carro parado em uma marcha alta, o que faria o motor apagar.
+            if (Speed < 10 && Marcha > 3)
+            {
+                Rpm = 0;
+                Speed = 0;
+                Console.WriteLine("Você tentou amdar com o carro em uma marcha alta, o motor apagou!");
+                return;
+            }
+
+            //Aviso de Neutro
+            if (Marcha == 0)
+            {
+                Rpm = Math.Min(Rpm + 800, 7000);
+                Console.WriteLine("NEUTRO!!");
+                return;
+            }
+
+            Console.WriteLine("Acelerando!");
+
+            // Funcionamento
+
+            Rpm += (600 / Marcha);
+            Speed += (Marcha * 5);
+            Fuel -= 0.1;
+
+            if(Speed >= 180 && Speed < 181)
+            {
+                Console.WriteLine("VELOCIDADE MÁXIMA!!!");
+                Speed = 180;
+            }
+
+            if(Rpm >= 6000)
+            {
+                if(Marcha < 5)
+                {
+                    Console.WriteLine("RPM Alto. TROQUE DE MARCHA!");
+                }
+                else
+                {
+                    Rpm = 6500;
+                    Console.WriteLine("RPM no limite! Não é possível subir mais a marcha.");
+                }
+            }
+            Console.WriteLine($"Status: {Marcha}ª Marcha | Velocidade: {Speed} km/h | RPM: {Rpm}");
+        }
+
+        public async Task FrearCarro()
+        {
+            bool parado = false;
+            while(! parado)
+            {
+                Console.WriteLine("Freando!");
+                if (Speed == 0)
+                {
+                    Console.WriteLine("\nCarro já está parado.");
+                    parado = true;
+                }
+                else
+                {
+                    while (Speed >= 0)
+                    {
+                        Speed -= 2;
+                        Rpm -= 100;
+                        Console.WriteLine($"Velocidade: {Speed} km/h");
+                        await Task.Delay(50);
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            // Lógica simples de alerta
+            string statusTemp = Temp > 100 ? "!!! SUPERAQUECIMENTO !!!" : "Normal";
+            string statusFuel = Fuel < 5 ? "!!! RESERVA !!!" : "OK";
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("\n>>>> PAINEL DE CONTROLE <<<<");
+            sb.AppendLine($"MARCHA: {Marcha}");
+            sb.AppendLine($"VELOCIDADE: {Speed} km/h");
+            sb.AppendLine($"RPM: {Rpm} rpm");
+            sb.AppendLine($"TEMP: {Temp:F1}°C ({statusTemp})");
+            sb.AppendLine($"COMBUSTÍVEL: {Fuel:F2}L ({statusFuel})");
+            sb.AppendLine("----------------------------");
+
+            return sb.ToString();
+        }
+
     }
 }
