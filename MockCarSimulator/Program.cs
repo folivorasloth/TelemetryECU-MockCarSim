7d8a7bae8a764372d43a02cf0e79e6b0e66a4b86
joao.falcao <joao.falcao@JOAOFALCAO>
2026-03-17 09:34:24 -0300

    Adicionar arquivos de projeto.

---
+++ MockCarSimulator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TelemetryECU
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("TelemetryECU | Mock Car Simulator");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Comandos:\nL - Ligar carro\nA - Acelerar\nF - Frear\nS - Sair do simulador.");
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Informações do carro:");
+                Console.WriteLine("RPM: " + rpm);
+                Console.WriteLine("Temperatura: " + temp);
+                Console.WriteLine("Velocidade: " + speed);
+                Console.WriteLine("Tanque combustível: " + fuel + "L");
+
+                var key = await Task.Run(() => Console.ReadKey(true).Key);
+
+                switch (key)
+                {
+                    case ConsoleKey.L:
+                        await LigarCarro();
+                        break;
+                    case ConsoleKey.A:
+                        await AcelerarCarro();
+                        break;
+                    case ConsoleKey.F:
+                        await FrearCarro();
+                        break;
+                    case ConsoleKey.S:
+                        running = false;
+                        Console.WriteLine("Saindo do simulador...");
+                        break;  
+                }
+            }
+        }
+    }
+}
---
+++ MockCarSimulator/Tasks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MockCarSimulator
+{
+    public class TasksCarro
+    {
+        public int Rpm { get; set; }
+        public double Temp { get; set; }
+        public double Fuel { get; set; }
+        public int Speed { get; set; }
+
+        public TasksCarro(int rpm, double temp, double fuel, int speed)
+        {
+            Rpm = rpm;
+            Temp = temp;
+            Fuel = fuel;
+            Speed = speed;
+        }
+
+        public async Task LigarCarro()
+        {
+            Console.WriteLine("\nLigando carro");
+            await Task.Delay(5000);
+            Rpm = 900;
+            Temp = 70;
+
+        }
+    }
+}
