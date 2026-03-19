65ae59558f592d3abea2bf0776d2239da6d824d1
joao.falcao <joaopedro35215745@gmail.com>
2026-03-19 10:07:12 -0300

    Added ASCII Logo & Enter Menu

--- MockCarSimulator/Program.cs
+++ MockCarSimulator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MockCarSimulator;
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TelemetryECU
@@ -11,10 +13,67 @@ namespace TelemetryECU
 
             TasksCarro carro = new TasksCarro(0, 30, 50, 0);
 
-            Console.WriteLine("|-----------------------------------------------|");
-            Console.WriteLine("|     PRESSIONE QUALQUER TECLA PARA INICIAR     |");
-            Console.WriteLine("|_______________________________________________|");
+            string asciiArt = @"
+  __  __            _        _____              _____ _                 _       _             
+ |  \/  |          | |      / ____|            / ____(_)               | |     | |            
+ | \  / | ___   ___| | __  | |     __ _ _ __  | (___  _ _ __ ___  _   _| | __ _| |_ ___  _ __ 
+ | |\/| |/ _ \ / __| |/ /  | |    / _` | '__|  \___ \| | '_ ` _ \| | | | |/ _` | __/ _ \| '__|
+ | |  | | (_) | (__|   <   | |___| (_| | |     ____) | | | | | | | |_| | | (_| | || (_) | |   
+ |_|  |_|\___/ \___|_|\_\   \_____\__,_|_|    |_____/|_|_| |_| |_|\__,_|_|\__,_|\__\___/|_|                                                                                                                                                                                              
+        ";
 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(asciiArt);
+            Console.ResetColor();
+
+            int boxStartLine = Console.CursorTop;
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+
+            Stopwatch timer = Stopwatch.StartNew();
+            bool isRed = false;
+            Console.CursorVisible = false;
+
+            while (!Console.KeyAvailable)
+            {
+                // Posiciona o cursor sempre no início do bloco da mensagem
+                Console.SetCursorPosition(x, y);
+
+                if (timer.ElapsedMilliseconds > 5000)
+                {
+                    // MODO ALERTA (PISCANTE)
+                    Console.ForegroundColor = isRed ? ConsoleColor.Red : ConsoleColor.DarkGray;
+
+                    Console.WriteLine(" ╔════════════════════════════════════════════════╗");
+                    Console.WriteLine(" ║                                                ║");
+                    Console.WriteLine(" ║           [ PRESS ANY KEY TO ENTER ]           ║");
+                    Console.WriteLine(" ║                                                ║");
+                    Console.WriteLine(" ╚════════════════════════════════════════════════╝");
+
+                    isRed = !isRed;
+                    Thread.Sleep(500); // Velocidade do pisca
+                }
+                else
+                {
+                    // MODO PADRÃO (ESTÁTICO)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(" ╔════════════════════════════════════════════════╗");
+                    Console.WriteLine(" ║                                                ║");
+                    Console.WriteLine(" ║           [ PRESS ANY KEY TO ENTER ]           ║");
+                    Console.WriteLine(" ║                                                ║");
+                    Console.WriteLine(" ╚════════════════════════════════════════════════╝");
+
+                    // Um delay menor aqui para não consumir 100% da CPU
+                    Thread.Sleep(100);
+                }
+            }
+
+            Console.ReadKey(true);
+            Console.ResetColor();
+            Console.SetCursorPosition(0, boxStartLine + 5); // Pula o box para continuar
+            TextoTopo();
+
+            // AQUI INICIA O PROGRAMA
             bool running = true;
 
             while (running)
